6d58d41222fd21f1e481eb69b1077905869512b3
Mohammed Emad <aboelkhier2000@gmail.com>
2022-06-17 01:25:02 +0200

    Committing my project

---
+++ SportsApp(Swift Project)/Leagues/Model/League.swift
@@ -0,0 +1,21 @@
+//
+//  League.swift
+//  SportsApp(Swift Project)
+//
+//  Created by Mohammed Emad Aboelkhier  on 15/06/2022.
+//
+
+import Foundation
+
+struct league : Decodable
+{
+    var strSport : String
+    var strLeague :  String
+    var strBadge  : String
+    var strYoutube : String
+}
+
+struct LeagueResults  : Decodable
+{
+    var countries : [league]
+}
